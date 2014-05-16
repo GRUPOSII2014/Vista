f2ff9b323cf8c4c1ebe8b161a5833714f4c5f413
PyRoS <cautivo.chacon@gmail.com>
2014-05-16 18:44:40 +0200

    Cambios metodo redireccion a cambiar contraseña

--- PrincipalControllerBeans.java
+++ PrincipalControllerBeans.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Vista;
 
 import Ejb.PersonaEjb;
 import Entidades.Alerta;
+import Entidades.Mensaje;
 import Entidades.Persona;
 import java.util.List;
 import javax.ejb.EJB;
@@ -25,18 +25,17 @@ public class PrincipalControllerBeans {
 
     @EJB
     private PersonaEjb persona;
-    
+
     @ManagedProperty(value = "#{loginController}")
     private LoginController session;
 
-    public PrincipalControllerBeans() {        
+    public PrincipalControllerBeans() {
     }
 
-    
     public Persona getPersona() {
         return persona.getPersona(session.getNss());
     }
-    
+
     public String verMiHistoria(Integer nss) {
         session.setBuscado(nss);
         return "HistoriaClinica.xhtml";
@@ -49,28 +48,27 @@ public class PrincipalControllerBeans {
     public void setSession(LoginController session) {
         this.session = session;
     }
-    
-    public String verMiTrabajo(Integer nss){
+
+    public String verMiTrabajo(Integer nss) {
         session.setBuscado(nss);
         return "Trabajo.xhtml";
     }
-    
-    public String verMisCitas (Integer nss){
+
+    public String verMisCitas(Integer nss) {
         session.setBuscado(nss);
         return "citas.xhtml";
     }
-    
+
     public List<Alerta> getAlertas() {
         return persona.allAlertas(session.getNss());
     }
-    
-<<<<<<< HEAD
-    public String cambiaPass(Integer nss){
+
+    public String cambiaPass(Integer nss) {
         session.setBuscado(nss);
         return "Cambiarclave.xhtml";
-=======
+    }
+
     public List<Mensaje> getMensajes() {
         return persona.allMensajes(session.getNss());
->>>>>>> de3fa7cf7ebdb01a8fbcab4a5eca94b6ddb98411
     }
 }
