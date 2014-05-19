e89dee96b4084897c2a06eebe426ec196c0ce544
Angel <laperla2@hotmail.es>
2014-05-19 19:13:49 +0200

    Cita, diagnostico, ingreso....

--- TratamientoBeans.java
+++ TratamientoBeans.java
@@ -6,13 +6,16 @@
 package Vista;
 
 import Ejb.IngresoEjb;
+import Ejb.PersonaEjb;
 import Entidades.Cantidad;
-import Entidades.Enumerados;
+import static Entidades.HistoriaClinica_.persona;
+import Entidades.Persona;
 import Entidades.Tratamiento;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.inject.Inject;
 
 
 @RequestScoped
@@ -21,74 +24,24 @@ public class TratamientoBeans {
 
     private Tratamiento tratamiento;
     private List <Cantidad> listaCant;
-    
-    @Inject
+    private Persona persona;
+    @EJB
     private IngresoEjb ejb;
+    @EJB
+    private PersonaEjb pers;
+    @ManagedProperty(value = "#{loginController}")
+    private LoginController login;
+    
     
     public TratamientoBeans() {
-       /* t=new Tratamiento();
-        listaCant= new ArrayList<Cantidad>();
-        
-        t.setNombre("Tratamiento 1");
-        
-        
-        
-        
-        t.setDescripcion("Limpieza y cura de las heridas cutáneas del paciente,"
-                + "cambio de suero cada 8 horas.");
-        Date inicio = new Date(2014, 05, 02);
-        t.setFechaInicio(inicio);
-        Date fin = new Date(2014, 05, 30);
-        t.setFechaFinalizacion(fin);
-        
-        
-        t.setTipo(Enumerados.tipoTratamiento.CURA);
-
-        
-
-        Medicamento m1 = new Medicamento();
-        m1.setId(1);
-        m1.setNombre("Ibuprofeno");
-        m1.setDescripcion(" antiinflamatorio no esteroideo (AINE), utilizado frecuentemente como"
-                + " antipirético y para el alivio sintomático del dolor de cabeza (cefalea), dolor "
-                + "dental (odontalgia), dolor muscular o mialgia, molestias de la menstruación "
-                + "(dismenorrea), dolor neurológico de carácter leve y dolor postquirúrgico");
-        
-        Medicamento m2 = new Medicamento();
-        m2.setId(2);
-        m2.setNombre("Lexatin");
-        m2.setDescripcion("fármaco con acción depresora del sistema nervioso central, destinado"
-                + " a disminuir o eliminar los síntomas de la ansiedad.");
-        
-        Medicamento m3 = new Medicamento();
-        m3.setNombre("Maalox");
-        m3.setId(3);
-        m3.setDescripcion(" derivado del aluminio y magnesio con bajo contenido en sodio. "
-                + "Posee una elevada actividad neutralizando el ácido clorhídrico que se produce"
-                + " en el estómago y que es el que provoca la acidez, lo que también reduce la "
-                + "formación y la actividad de la enzima pepsina que segrega el estómago.");
-        
-        List<Medicamento> medicamentos = new ArrayList<>();
-        medicamentos.add(m1);
-        medicamentos.add(m2);
-        medicamentos.add(m3);
-        
-        Cantidad c1 = new Cantidad();
-        c1.setMedicamento(m1);
-        c1.setCantidad((float)3);
-        Cantidad c2 = new Cantidad();
-        c2.setMedicamento(m2);
-        c2.setCantidad((float)2);
-        Cantidad c3 = new Cantidad();
-        c3.setMedicamento(m3);
-        c3.setCantidad((float)1);
-        listaCant.add(c1);
-        listaCant.add(c2);
-        listaCant.add(c3);*/
-        
-        
     }
 
+    public boolean inic(){
+        persona = pers.getPersona(login.getBuscado());
+        tratamiento = persona.getTratamiento().get(persona.getTratamiento().size()-1);
+        listaCant = tratamiento.getCantidades();
+        return true;
+    }
     /**
      * @return the t
      */
@@ -120,6 +73,38 @@ public class TratamientoBeans {
     public String terminarTratamiento(){
         ejb.terminarTratamiento(tratamiento);
         return "Trabajo.xhtml";
+    }
+
+    public IngresoEjb getEjb() {
+        return ejb;
+    }
+
+    public void setEjb(IngresoEjb ejb) {
+        this.ejb = ejb;
+    }
+
+    public PersonaEjb getPers() {
+        return pers;
+    }
+
+    public void setPers(PersonaEjb pers) {
+        this.pers = pers;
+    }
+
+    public LoginController getLogin() {
+        return login;
+    }
+
+    public void setLogin(LoginController login) {
+        this.login = login;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
     }
 
     
